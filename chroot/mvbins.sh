5cba9cebd9208ae865b9e11409b7157627542b5e
icebowl <icebowl1967@gmail.com>
2019-03-18 05:19:25 -0700

    AA

---
+++ chroot/mvbins.sh
@@ -0,0 +1,8 @@
+#mkdir /srv/chroot;
+
+
+cp /lib/x86_64-linux-gnu/libtinfo.so.5 lib/;
+cp /lib/x86_64-linux-gnu/libdl.so.2 lib/;
+cp /lib/x86_64-linux-gnu/libc.so.6 lib/;
+cp /lib64/ld-linux-x86-64.so.2 lib64/;
+cp /bin/bash bin/;
