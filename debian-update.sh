7161ddaf1f0ea292df4e2cea77f65503e0a11a8f
icebowl <icebowl1967@gmail.com>
2019-06-06 03:51:11 -0700

    AA

--- debian-update.sh
+++ debian-update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #system update
-apt-get update -y;
-apt-get upgrade  -y;
-apt-get install curl;
-apt-get install git;
-apt-get install net-tools;
-apt-get install geany geany-plugins sudo chromium -y;
+apt update -y;
+apt upgrade  -y;
+apt install curl;
+apt install git;
+apt install net-tools;
+apt install geany geany-plugins sudo chromium -y;
 apt  install build-essential gcc g++ make perl dkms;
 update gcc;
 update g++;
