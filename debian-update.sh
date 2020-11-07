ed1136145ec5f6f1e128f41f6905c5a2c375e001
icebowl <icebowl1967@gmail.com>
2020-11-07 07:24:56 -0800

    AA

--- debian-update.sh
+++ debian-update.sh
@@ -2,14 +2,14 @@
 #system update
 apt update -y;
 apt upgrade  -y;
-apt install curl;
-apt install git;
-apt install net-tools;
+apt install curl -y;
+apt install git -y ;
+apt install net-tools -y;
 apt install geany geany-plugins sudo chromium -y;
-apt  install build-essential gcc g++ make perl dkms;
+apt  install build-essential gcc g++ make perl dkms -y;
 update gcc;
 update g++;
-apt install linux-headers-$(uname -r);
-apt install locate;
+apt install linux-headers-$(uname -r) -y;
+apt install locate -y;
 updatedb;
-dpkg-reconfigure console-setup;
+#dpkg-reconfigure console-setup;
--- set-background-color.sh
+++ set-background-color.sh
@@ -1,3 +1,3 @@
 gsettings set org.gnome.desktop.background picture-uri none
-gsettings set org.gnome.desktop.background primary-color '#FF0000'
+gsettings set org.gnome.desktop.background primary-color '#7AB09F'
 gsettings set org.gnome.desktop.background color-shading-type 'solid'
